62fe85b344f8a837eb3ef0135a2d394b6ccba84d
DeliaOana <delia.oana1995@gmail.com>
2020-04-02 12:36:02 +0300

    Update sensehat_mqtt.py

--- sensehat_mqtt.py
+++ sensehat_mqtt.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env.python
 import math
 import time
@@ -10,14 +11,13 @@ sense = SenseHat()
 MQTT_HOST = 'mqtt.beia-telemetrie.ro'
 MQTT_TOPIC = 'odsi/vlc'
 
+
 while True: 
-	payload_dict = {"TEMP" :  sense.get_temperature(),
+    payload_dict = {"TEMP" :  sense.get_temperature(),
                 	"HUMID" : sense.get_humidity(),
-                	"PRESS" : sense.get_pressure()
-            	        }
-        try:
-            publish.single(MQTT_TOPIC, qos = 1, hostname = MQTT_HOST, payload = json.dumps(payload_dict))
-        except:
-            time.sleep(0.01) #TODO local storage 
-	
-	time.sleep(10) 
+                	"PRESS" : sense.get_pressure()}
+    try:
+        publish.single(MQTT_TOPIC, qos = 1, hostname = MQTT_HOST, payload = json.dumps(payload_dict))
+    except:
+        time.sleep(0.01) #TODO local storage 
+    time.sleep(10) 
